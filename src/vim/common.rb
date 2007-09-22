655f72122020f7c63ce77cdcb0804004ec626d76
Stefano Zacchiroli <zack@debian.org>
2007-09-22 18:03:12 +0000

    bumping license header to GPL v3

--- src/vim/common.rb
+++ src/vim/common.rb
@@ -2,10 +2,10 @@
 #
 # Copyright (C) 2007 Stefano Zacchiroli
 #
-# This program is free software, you can redistribute it and/or modify it under
-# the terms of the GNU General Public License version 2 as published by the
-# Free Software Foundation; either version 2 of the License, or (at your
-# option) any later version.
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # $Id: common.rb 866 2007-01-24 21:27:58Z zack $
 
